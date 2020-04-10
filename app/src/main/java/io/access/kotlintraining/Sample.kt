921939dbd0c0e2640ddaefc8e600143d16d736e9
Ikebe Soichi <souichi1997@gmail.com>
2020-04-10 09:56:46 +0900

    answered

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -1,7 +1,141 @@
 package io.access.kotlintraining
 
+import java.util.*
+
 fun welcome() {
     println("welcome to ACCESS!")
 }
 
-// TODO: declare hello function
+fun hello() {
+    println("Hello World!")
+}
+
+// Exercise1-1
+fun fizzBuzz(num: Int): String {
+    return when {
+        num % 15 == 0 -> "FizzBuzz"
+        num % 3 == 0 -> "Fizz"
+        num % 5 == 0 -> "Buzz"
+        else -> "$num"
+    }
+}
+
+// Exercise1-2
+fun isLeapYear(y: Int): Boolean {
+    return if (y > 0) {
+        when {
+            y % 100 == 0 && y % 400 != 0 -> false
+            y % 4 == 0 -> true
+            else -> false
+        }
+    } else {
+        println("inputs must be positive.")
+        return false
+    }
+}
+
+// Exercise1-3
+fun power(a: Int, n: Int): Long {
+    if (a < 0 || n < 0) {
+        println("inputs must be positive. return 0")
+        return 0
+    }
+    var result = a.toLong()
+    for (i in 1 until n) {
+        result *= a.toLong()
+    }
+    return result
+}
+
+// Exercise2-1
+fun Int.isEven() = this % 2 == 0
+
+fun Int.isOdd() = this % 2 != 0
+
+// Exercise2-2
+fun rollDice() {
+    val d = Dice(16)
+    for (i in 1..100) {
+        println("Dice${i}: ${d.roll()}") // 1〜16 までの数字
+    }
+    println(d.roll()) // Exception
+}
+
+class Dice(private val n: Int) {
+    private var counter: Int = 0
+
+    fun roll(): Int {
+        if (counter++ >= 100) throw Exception("I was broken.")
+        val random = Random()
+        return random.nextInt(n) + 1
+    }
+}
+
+// Exercise2-3
+fun customSetter() {
+    val p = MyCustomClass()
+    p.propertyWithCounter = 123
+    p.propertyWithCounter = 456
+    p.propertyWithCounter = 789
+    println(p.counter) // 3
+}
+
+class MyCustomClass {
+    var counter: Int = 0
+    var propertyWithCounter: Int = 0
+        set(value) {
+            field = value
+            counter++
+        }
+}
+
+// Exercise2-4
+fun runNabeatsu() {
+    val n = NabeAtsu()
+    for (i in 1..100) {
+        println(n.next())
+    }
+}
+
+class NabeAtsu {
+    private var counter: Int = 0
+
+    fun next(): String {
+        counter++
+        return if (counter % 3 == 0 || counter.toString().contains("3")) {
+            "Aho"
+        } else {
+            counter.toString()
+        }
+    }
+}
+
+
+// Exercise3
+class Client(val personalInfo: PersonalInfo?)
+
+class PersonalInfo(val email: String?)
+
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+class CosmosMailer : Mailer {
+    override fun sendMessage(email: String, message: String) {
+        println("To: $email\nmessage: $message")
+    }
+}
+
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    message ?: return
+    client?.personalInfo?.email?.let {
+        mailer.sendMessage(it, message)
+    }
+}
+
+fun executeMail() {
+    val personalInfo: PersonalInfo = PersonalInfo("antonio@abc-company.com")
+    val client: Client = Client(personalInfo)
+    val mailer: Mailer = CosmosMailer()
+    sendMessageToClient(client, "Hello, Antonio!", mailer)
+}
--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -16,6 +16,69 @@ class SampleTest {
 
     @Test
     fun testHello() {
-        // TODO: implement to call hello function
+        hello()
+    }
+
+    @Test
+    fun testFizzBuzz() {
+        // assertEquals 等の assert 系 function を使おう
+        // 以下の kotlin.test パッケージのリファレンスも参照のこと
+        // https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/index.html
+
+        // 引数が3の倍数の時はFizzを出力
+        assertEquals(expected = "Fizz", actual = fizzBuzz(3))
+        // 引数が5の倍数の時はBuzzを出力
+        assertEquals(expected = "Buzz", actual = fizzBuzz(5))
+        // 引数が3の倍数かつ5の倍数のときはFizzBuzz出力
+        assertEquals(expected = "FizzBuzz", actual = fizzBuzz(15))
+        // どれでもない時は数字を出力
+        assertEquals(expected = "7", actual = fizzBuzz(7))
+    }
+
+    @Test
+    fun testLeapYear() {
+        // 閏年
+        assertEquals(expected = true, actual = isLeapYear(2000))
+        // 閏年ではない(100で割り切れてかつ400で割り切れない)
+        assertEquals(expected = false, actual = isLeapYear(1700))
+        // 閏年ではない(その他)
+        assertEquals(expected = false, actual = isLeapYear(2001))
+    }
+
+    @Test
+    fun testPower() {
+        assertEquals(expected = 1, actual = power(1, 1))
+        assertEquals(expected = 4, actual = power(2, 2))
+        assertEquals(expected = 27, actual = power(3, 3))
+    }
+
+    @Test
+    fun testOddEven() {
+        // 偶数
+        assertEquals(expected = true, actual = 2.isEven())
+        assertEquals(expected = false, actual = 2.isOdd())
+        // 奇数
+        assertEquals(expected = true, actual = 3.isOdd())
+        assertEquals(expected = false, actual = 3.isEven())
+    }
+
+    @Test
+    fun testDice() {
+        rollDice()
+    }
+
+    @Test
+    fun testCustomSetter() {
+        customSetter()
+    }
+
+    @Test
+    fun testNabeatsu() {
+        runNabeatsu()
+    }
+
+    @Test
+    fun testMail() {
+        executeMail()
     }
 }
